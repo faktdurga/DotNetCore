becd15b3725509a3b7b72c3e33118bea51f663ac
Durgaprasad Mohite <78316362+faktdurga@users.noreply.github.com>
2024-12-18 22:34:18 +0530

    Add files via upload

---
+++ Controller and IActionMethod/BankApplicationAssignment/BankApplicationAssignment/Controller/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using BankApplicationAssignment.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankApplicationAssignment.Controller
+{
+    public class HomeController : ControllerBase
+    {
+        [Route("Home")]
+        public IActionResult Index()
+        {
+            Response.StatusCode = 200;
+            return Content("Welcome to best bank");
+        }
+
+        [Route("account-details")]
+        public IActionResult GetAccountDetails()
+        {
+            UserDetails UD = new UserDetails() { 
+                AccountNumber= 1000,
+                AccountHolderName = "Durgaprasad Mohite",
+                Balance= 25000,
+            };
+
+
+            return Ok(UD);
+        }
+
+        [Route("account-statement")]
+        public IActionResult GetBankStatement()
+        {
+            return new VirtualFileResult("/BankStatement.pdf", "application/pdf");
+        }
+
+        [Route("get-current-balance/{acountnumber:int}")]
+        public IActionResult GetBankAccount(int acountnumber)
+        {
+            UserDetails UD = new UserDetails()
+            {
+                AccountNumber = 1000,
+                AccountHolderName = "Durgaprasad Mohite",
+                Balance = 25000,
+            };
+
+
+            if(acountnumber != 1000) 
+            {
+                return BadRequest("Accoutn number should be 1000");
+            }
+
+
+            return Ok(UD.Balance);
+        }
+
+        [Route("get-current-balance")]
+        public IActionResult GetBankAccount()
+        {
+            
+            return NotFound("Account number should be supplied");
+            
+        }
+    }
+}
---
+++ Controller and IActionMethod/BankApplicationAssignment/BankApplicationAssignment/Model/UserDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace BankApplicationAssignment.Model
+{
+    public class UserDetails
+    {
+        public int AccountNumber { get; set; }
+        public string? AccountHolderName { get; set; }
+        public int Balance { get; set; }
+
+    }
+}
