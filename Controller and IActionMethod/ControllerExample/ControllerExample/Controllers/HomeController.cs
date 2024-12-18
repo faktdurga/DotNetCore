0e5e3ad7c8dec9a0dcf0cecaf316ceca984f66fa
Durgaprasad Mohite <78316362+faktdurga@users.noreply.github.com>
2024-12-18 22:36:32 +0530

    Add files via upload

---
+++ Controller and IActionMethod/ControllerExample/ControllerExample/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using ControllerExample.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public ContentResult Index()
+        {
+            return Content("<h1>Hello from index</h1>", "text/html");
+        }
+
+        [Route("about")]
+        public string About()
+        {
+            return "Hello from About";
+        }
+
+        [Route("contactus")]
+        public string Contact()
+        {
+            return "Hello from contact us";
+        }
+
+        [Route("person")]
+        public JsonResult Person()
+        {
+            Person p = new Person();
+
+            p.Id = 1;
+            p.FirstName = "Durga";
+            p.LastName = "Mohite";
+            p.Age = 35;
+
+            return new JsonResult(p);
+        }
+        //C:\Users\DURGA\Downloads
+        [Route("file")]
+        public VirtualFileResult File()
+        {
+            return new VirtualFileResult("/Top-500-DotNet-Interview-Questions-2022-PDF (1).pdf", "application/pdf");
+        }
+
+        [Route("physcialfile")]
+        public PhysicalFileResult PhysicalFile()
+        {
+            return new PhysicalFileResult(@"C:\Users\DURGA\Downloads\Table Invoice.pdf", "application/pdf");
+        }
+
+        [Route("filebyte")]
+        public FileContentResult ByteFile()
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(@"C:\Users\DURGA\Downloads\Table Invoice.pdf");
+            return new FileContentResult(bytes, "application/pdf");
+        }
+    }
+}
---
+++ Controller and IActionMethod/ControllerExample/ControllerExample/Program.cs
@@ -0,0 +1,11 @@
+using ControllerExample.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
