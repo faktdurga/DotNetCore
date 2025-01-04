dc94e21baeb3a900381ef17aec6da6a358d004eb
Durgaprasad Mohite <78316362+faktdurga@users.noreply.github.com>
2025-01-04 22:11:48 +0530

    Add files via upload

---
+++ Section 9 Layout Views MVC/LayoutViewsExample/LayoutViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/about-us")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
+        [Route("/contact-us")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Section 9 Layout Views MVC/LayoutViewsExample/LayoutViewsExample/Controllers/ProductsController1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewsExample.Controllers
+{
+    public class ProductsController1 : Controller
+    {
+        [Route("Product")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("search-product/{productid?}")]
+
+        public IActionResult Search(int? productid) 
+        {
+            ViewBag.productid = productid;
+            return View();
+        }
+
+        [Route("order-product")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+
+    }
+}
