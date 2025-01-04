a7227b167ccdcc5bfdf77c659f92ecd6c0d23e6c
Durgaprasad Mohite <78316362+faktdurga@users.noreply.github.com>
2025-01-04 12:03:20 +0530

    Add files via upload

---
+++ Section 8 Razor Views MVC/WeatherApplication/WeatherApplication/Controller/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApplication.Models;
+
+namespace WeatherApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+           List<City> cities = new List<City>()
+           {
+               new City{CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),  Temprature = 33},
+               new City{CityUniqueCode = "NYC", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),  Temprature = 60},
+               new City{CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),  Temprature = 82}
+           };
+
+
+            return View(cities);
+        }
+
+        [Route("/weather/{citycode}")]
+        public IActionResult CityDetails(string citycode)
+        {
+            List<City> cities = new List<City>()
+           {
+               new City{CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),  Temprature = 33},
+               new City{CityUniqueCode = "NYC", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),  Temprature = 60},
+               new City{CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),  Temprature = 82}
+           };
+
+            City? city = cities.FirstOrDefault(x => x.CityUniqueCode == citycode);
+
+            return View(city);
+        }
+    }
+}
