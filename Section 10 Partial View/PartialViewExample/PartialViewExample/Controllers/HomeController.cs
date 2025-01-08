4ed90c3122dd3fd0d205d4388fe4f8fdab8eda6f
Durgaprasad Mohite <78316362+faktdurga@users.noreply.github.com>
2025-01-08 07:12:01 +0530

    Add files via upload

---
+++ Section 10 Partial View/PartialViewExample/PartialViewExample/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewExample.Models;
+
+namespace PartialViewExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("programming-language")]
+        public IActionResult ProgramingLanguages()
+        {
+            ListModel listModel = new ListModel();
+
+            listModel.ListTitle = "Programing Language";
+            listModel.ListItems = new List<string>() { "Java", "C#", "Python" };
+
+
+            return PartialView("_ListPartialViews", listModel);
+            //return new PartialViewResult() { ViewName ="_ListPartialView", Model =listModel }; 
+        }
+    }
+}
