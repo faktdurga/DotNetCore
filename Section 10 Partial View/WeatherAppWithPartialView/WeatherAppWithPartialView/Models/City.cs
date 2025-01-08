c3d79ad58eb05ee96857e3b8952c2b57418a7877
Durgaprasad Mohite <78316362+faktdurga@users.noreply.github.com>
2025-01-08 07:14:53 +0530

    Add files via upload

---
+++ Section 10 Partial View/WeatherAppWithPartialView/WeatherAppWithPartialView/Models/City.cs
@@ -0,0 +1,12 @@
+﻿namespace WeatherAppWithPartialView.Models
+{
+    public class City
+    {
+       
+            public string? CityUniqueCode { get; set; }
+            public string? CityName { get; set; }
+            public DateTime? DateAndTime { get; set; }
+            public int Temprature { get; set; }
+        
+    }
+}
