913bb2f8bc3b1865da40dc68722cdf45973bc91a
Durgaprasad Mohite <78316362+faktdurga@users.noreply.github.com>
2025-01-15 15:24:37 +0530

    Add files via upload

---
+++ Section 12 Dependency Injection/01. Services/ServiceContracts/ICitiesService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts
+{
+    public interface ICitiesService
+    {
+        Guid ServiceInstanceID { get; }
+        List<string> GetCountries();
+
+        List<string> GetCities();
+    }
+}
