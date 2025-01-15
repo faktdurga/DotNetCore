a2a629b03d52e36f5aa6fc9814d4bbd77f8792a1
Durgaprasad Mohite <78316362+faktdurga@users.noreply.github.com>
2025-01-15 15:25:26 +0530

    Add files via upload

---
+++ Section 12 Dependency Injection/01. Services/Services/CitiesService.cs
@@ -0,0 +1,60 @@
+﻿using ServiceContracts;
+
+namespace Services
+{
+  public class CitiesService : ICitiesService, IDisposable
+  {
+        private List<string> _cities;
+        private List<string> _countries;
+
+        private Guid _serviceInstanceID;
+        public Guid ServiceInstanceID
+        {
+
+            get 
+            { 
+                return _serviceInstanceID;
+            } 
+        }
+
+
+
+        public CitiesService()
+        {
+                _serviceInstanceID = Guid.NewGuid();
+                
+                _cities = new List<string>() { 
+                  "London",
+                  "Paris",
+                  "New York",
+                  "Tokyo",
+                  "Rome"
+                };
+
+                _countries = new List<string>()
+                {
+                    "UK",
+                    "France",
+                    "USA",
+                    "Japan",
+                    "Italy"
+                };  
+
+        }
+
+        public List<string> GetCities()
+        {
+          return _cities;
+        }
+
+        public List<string> GetCountries()
+        {
+              return _countries;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
