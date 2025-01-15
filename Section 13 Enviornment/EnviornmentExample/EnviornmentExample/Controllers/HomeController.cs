a465aaa444ac34a8bf3b9615681a60c804fe03c6
Durgaprasad Mohite <78316362+faktdurga@users.noreply.github.com>
2025-01-16 05:10:16 +0530

    Add files via upload

---
+++ Section 13 Enviornment/EnviornmentExample/EnviornmentExample/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnviornmentExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [Route("/")]
+        [Route("/some-route")]
+        public IActionResult Index()
+        {
+            ViewBag.CurrentEnviornment = _webHostEnvironment.EnvironmentName;
+            return View();
+        }
+
+        [Route("/some-route")]
+        public IActionResult Other()
+        {
+            return View();
+        }
+    }
+}
